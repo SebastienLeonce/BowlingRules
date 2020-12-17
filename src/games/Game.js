2074e08d277807596aaf1fa61c8c9dc7b92ca672
Sébastien LEONCE <sebastien.leonce@cea.fr>
2020-12-17 15:38:41 +0100

    Model du jeux

---
+++ src/games/Game.js
@@ -0,0 +1,77 @@
+import User from './User';
+import ObjUser from './User';
+
+export default class Game {
+    constructor () {
+        this.Users = [];
+    }
+
+    addPlayer(Username) {
+        this.Users.push(new ObjUser(Username));
+    }
+}
+
+export default class Score {
+    constructor () {
+        this.premierLance = 0;
+        this.secondLance  = 0;
+        this.premierLanceDouble = false;
+        this.secondLanceDouble  = false;
+        this.tourCourant        = 1
+    }
+
+    updatePremierLancer (score) {
+        this.premierLance = score;
+    }
+
+    updateSecondLancer (score) {
+        this.SecondLance = score;
+    }
+
+    isStrike() {
+        if (this.premierLance == 10) {
+            return true;
+        }
+        return false;
+    } 
+
+    isSpare() {
+        if (this.premierLance + this.secondLance == 10) {
+            return true;
+        }
+        return false;
+    }
+
+    getScoreTotal() {
+        let coeff1 = 1;
+        if (this.premierLanceDouble) coeff1++; 
+        let coeff2 = 1;
+        if (this.premierLanceDouble) coeff2++; 
+
+        return coeff1*this.premierLance + coeff2*this.secondLance
+    }
+
+    getScorePremierLance() {
+        return this.premierLance * (this.premierLanceDouble == true) ? 2 : 1;
+    }
+
+    getScoreSecondLance() {
+        return this.secondLance * (this.secondLanceDouble == true) ? 2 : 1;
+    }
+
+    setPremierLanceDouble() {
+        this.premierLanceDouble = true;
+    }
+
+    setSecondLanceDouble() {
+        this.premierLanceDouble = true;
+    }
+
+    tourSuivant() {
+        this.tourCourant++;
+        for (Users in User) {
+            User.tourCourant = this.tourCourant;
+        }
+    }
+
+}
---
+++ src/games/User.js
@@ -0,0 +1,69 @@
+import ObjScore from './Game';
+
+export default class User {
+    constructor(nom) {
+        this.nom         = nom;
+        this.score       = [];
+        this.tourCourant = 1;
+
+        for (let i = 0; i < 11; i++) {
+            this.score.push(new ObjScore())
+        }
+    }
+
+    ajouterScore1(score1) {
+        let n = this.tourCourant - 1;
+
+        this.score[n].updatePremierLancer(score1);
+        
+        if (this.score[n].isStrike()) {
+            this.score[n].updateSecondLancer(0);
+            this.score[n + 1].setPremierLanceDouble();
+            this.score[n + 1].setSecondLanceDouble();
+        }
+    }
+
+    ajouterScore2(score2) {
+        let n = this.tourCourant - 1;
+
+        this.score[n].updateSecondLancer(score1);
+
+        if (this.score[n].isSpare()) {
+            this.score[n + 1].setPremierLanceDouble();
+        }
+    }
+
+    isRelance2Available() {
+        let n = this.tourCourant - 1;
+        if (this.score[n].isStrike()) {
+            return false;
+        }
+        return true;
+    }
+
+    getScoreTotal() {
+        let n = this.tourCourant - 1;
+        return this.score[n].getScoreTotal();
+    }
+
+    getScorePremierLance() {
+        let n = this.tourCourant - 1;
+        return this.score[n].getScore1();
+    }
+
+    getScoreSecondLance() {
+        let n = this.tourCourant - 1;
+        return this.score[n].getScore2();
+    }
+
+    getNom() {
+        return this.getNom;
+    }
+
+    /*tourBonusAvailable() {
+        if (this.score[-2].isStrike() || this.score[-2].isSpare()) {
+            return true;
+        } 
+        return false;
+    }*/
+}
