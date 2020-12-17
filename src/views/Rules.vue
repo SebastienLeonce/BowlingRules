19b250e8ef07d37ccefde1d6170767250a519f38
alexisdacosta <pro.alexisdacosta@gmail.com>
2020-12-17 16:20:13 +0100

    #10 Affichage des règles

---
+++ src/views/Rules.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="Rules">
+    <router-link to="/game">Jeu</router-link> |
+    <router-link to="/rules">Règle</router-link> |
+    <router-link to="/settings">Paramètre</router-link>
+    <RulesText/>
+  </div>
+</template>
+
+<script>
+import RulesText from '@/components/RulesText.vue'
+export default {
+  name: 'Game',
+  components: {
+      RulesText
+  }
+}
+</script>
---
+++ src/views/Settings.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="Settings">
+      <router-link to="/game">Jeu</router-link> |
+      <router-link to="/rules">Règle</router-link> |
+      <router-link to="/settings">Paramètre</router-link>
+    <p>This is an Settings page</p>
+  </div>
+</template>
