12b54a2000480bb9a139dd6dd2704e9200372eaa
Sébastien LEONCE <sebastien.leonce@cea.fr>
2021-01-04 14:44:48 +0100

    Gestion des joueurs dans la classe Game

--- src/models/Game.js
+++ src/models/Game.js
@@ -2,8 +2,9 @@ import ObjUser from './User';
 
 export default class Game {
     constructor () {
-        this.Users = [];
-        this.tourCourant = 1;
+        this.Users         = [];
+        this.tourCourant   = 1;
+        this.joueurCourant = 0;
     }
 
     addPlayer(Username) {
@@ -14,6 +15,25 @@ export default class Game {
         this.tourCourant++;
         for (let i = 0; i < this.Users.length; i++) {
             this.Users[i].tourCourant = this.tourCourant;
+        }
+    }
+
+    joueurSuivant() {
+        if (this.tourCourant < 11) {
+            if (this.joueurCourant < this.Users.length) {
+                this.joueurCourant++;
+            } else {
+                this.joueurCourant = 0;
+                this.tourCourant++;
+                for (let i = 0; i < this.Users.length; i++) {
+                    this.Users[i].tourCourant = this.tourCourant;
+                }
+            }
+        } else if (this.tourCourant < 13) {
+            console.log("TODO gestion après le tour 10")
+            return -1;
+        } else {
+            return -1;
         }
     }
 }
