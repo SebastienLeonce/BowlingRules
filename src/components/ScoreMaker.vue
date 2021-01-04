c2528cca78a22844ab9a8ecb44768a15d9fffbf8
alexisdacosta <pro.alexisdacosta@gmail.com>
2021-01-04 17:09:19 +0100

    Fin de partie sur le Front et correction des bugs de Seb la

--- src/components/ScoreMaker.vue
+++ src/components/ScoreMaker.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="uk-card uk-card-hover uk-box-shadow-small uk-padding uk-overflow-auto uk-animation-scale-up" style="text-align: left;">
-        <p>SCORES<br><span style="font-size: 10pt;"> COURANT : {{$parent.$parent.gameObj.tourCourant}} | 
+        <p>SCORES<br><span style="font-size: 10pt;"> <span>COURANT : {{$parent.$parent.gameObj.tourCourant}}</span> | 
            JOUEUR COURANT : {{currentPlayer().nom}}</span> <br> {{error}}
         </p>
         <label v-if="score1Display === true" class="uk-form-label" for="form-horizontal-text">1er lancer</label>
@@ -11,13 +11,14 @@
         <div v-if="score2Display === true" class="uk-margin">
             <input id=score2 v-model="score2" class="uk-input uk-form" style="width: 30%;" type="text" placeholder="" autocomplete="off">
         </div>
-        <button class="uk-button uk-button-default" v-on:click="addScore()">Ajouter Score</button>
+        <button v-if="(score2Display === true || score1Display === true)" class="uk-button uk-button-default" v-on:click="addScore()">Ajouter Score</button>
+        <button v-if="score1Display === true" class="uk-button uk-button-default" v-on:click="addScore(10)">Strike</button>
     </div>
 </template>
 
 <script>
 // import UIkit from 'uikit';
-import Vue from 'vue'
+// import Vue from 'vue'
 
 export default {
     name: 'ScoreMaker',
@@ -44,38 +45,58 @@ export default {
             // return this.$parent.$parent.j
             return this.currentPlayer().lanceCourant;
         },
-        addScore(){
+        addScore(score){
             console.log(this.playerList.length)
             console.table(this.playerList)
 
-            console.log("Current Lancer : " + this.currentLancer())
-            if(this.$parent.$parent.gameObj.joueurCourant < this.playerList.length){
+            if(this.currentLancer() == 1 && score == 10){
+                try {
+                this.currentPlayer().ajouterScore1(parseInt(score))
+                let res = this.$parent.$parent.gameObj.joueurSuivant();
+                if (res == -1 || this.$parent.$parent.gameObj.tourCourant >= 13){
+                    alert("Fin de partie")
+                    this.score2Display = false
+                    this.score1Display = false
+                    this.error = "Fin de partie"
+                }
+                else{
+                    this.score2Display = false
+                    this.score1Display = true
+                    this.error = "Strike"
+                    this.score2 = ""
+                    this.score1 = ""
+                }
+                } catch (e) {
+                    this.error = e
+                }
+                
+            }
+            else if(this.$parent.$parent.gameObj.joueurCourant < this.playerList.length){
                 if (this.currentLancer() == 1){
                     try {
+                        this.currentPlayer().ajouterScore1(parseInt(this.score1))
+                        let res = this.$parent.$parent.gameObj.joueurSuivant();
+
                         if(this.score1 == 10){
-                            this.currentPlayer().ajouterScore1(parseInt(this.score1))
-                            this.$parent.$parent.gameObj.joueurSuivant();
-                            // this.currentPlayer().ajouterScore2(parseInt(0));
-                            // this.$parent.$parent.gameObj.joueurSuivant();
                             this.score2Display = false
                             this.score1Display = true
                             this.error = "Strike"
                             this.score2 = ""
                             this.score1 = ""
-
-                            if (this.$parent.$parent.gameObj.joueurCourant == this.playerList.length){
-                                for(let child in this.$parent.$childrens){
-                                    child.$forceUpdate()
-                                    Vue.forceUpdate()
-                                }
-                            }
                         }
                         else{
-                            this.currentPlayer().ajouterScore1(parseInt(this.score1))
-                            this.$parent.$parent.gameObj.joueurSuivant();
                             this.score2Display = true
                             this.score1Display = false
                             this.error = ""
+                            this.score2 = ""
+                            this.score1 = ""
+                        }
+
+                        if (res == -1 || this.$parent.$parent.gameObj.tourCourant >= 13){
+                            alert("Fin de partie")
+                            this.score2Display = false
+                            this.score1Display = false
+                            this.error = "Fin de partie"
                         }
                     } catch (e) {
                         this.error = e
@@ -89,22 +110,30 @@ export default {
 
                     try {
                         this.currentPlayer().ajouterScore2(parseInt(this.score2))
-                        this.$parent.$parent.gameObj.joueurSuivant();
+                        let res = this.$parent.$parent.gameObj.joueurSuivant();
+                        
                         this.score2Display = false
                         this.score1Display = true
                         this.error = ""
                         this.score2 = ""
                         this.score1 = ""
+
+                        if (res == -1 || this.$parent.$parent.gameObj.tourCourant >= 13){
+                            alert("Fin de partie")
+                            this.score2Display = false
+                            this.score1Display = false
+                            this.error = "Fin de partie"
+                        }
                     } catch (e) {
                         this.error = e
                     }   
 
-                    if (this.$parent.$parent.gameObj.joueurCourant == this.playerList.length){
-                        for(let child in this.$parent.$childrens){
-                            child.$forceUpdate()
-                            Vue.forceUpdate()
-                        }
-                    }
+                    // if (this.$parent.$parent.gameObj.joueurCourant == this.playerList.length){
+                    //     for(let child in this.$parent.$childrens){
+                    //         child.$forceUpdate()
+                    //         Vue.forceUpdate()
+                    //     }
+                    // }
                 }
                  
                 
